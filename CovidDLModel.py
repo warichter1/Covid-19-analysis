e7394411fb41e39cf2d2f23bf055b7c66c5b18f9
Bill Richter <wrichter1@yahoo.com>
2022-03-30 07:34:04 -0400

    Add Ba.2 dominant label

--- CovidDLModel.py
+++ CovidDLModel.py
@@ -154,6 +154,7 @@ def plotUS(inday, intoday, cdate, currentDate, cases, caseRate, growthRates,
     legend['Omicron Ba.1 Identified: 11/25'] = (674, 'Silver', lw)
     legend['Omicron Ba.1 Dominant 12/20'] = (699, 'Olive', lw)
     legend['Omicron Ba.2 Identified 1/21'] = (731, 'orange', lw)
+    legend['Omicron Ba.2 Dominant 3/29'] = (798, 'violet', lw)
     
     if showTotal is True:
         label, = plt.plot(cases,  color='red', label='Cases')
