7d845d85b4e0031976a6ff7ae6ada502073f5349
Bill Richter <wrichter1@yahoo.com>
2022-06-20 20:41:59 -0400

    Cleanup legend

--- CovidDLModel.py
+++ CovidDLModel.py
@@ -150,10 +150,10 @@ def plotUS(inday, intoday, cdate, currentDate, cases, caseRate, growthRates,
     legend['Delta Identified: 4/24'] = (443, 'purple', lw)
     legend['Delta Dominant: 6/5'] = (501, 'red', lw)
     legend['Vaccine Mandate: 9/9'] = (597, 'crimson', lw)
-    legend['Omicron Ba.1 Identified: 11/25'] = (674, 'Silver', lw)
     legend['Omicron Ba.1 Dominant 12/20'] = (699, 'Olive', lw)
     legend['Omicron Ba.2 Identified 1/21'] = (731, 'orange', lw)
     legend['Omicron Ba.2 Dominant 3/29'] = (798, 'violet', lw)
+    legend['Omicron Ba.2.12.1 Dominant 4/24'] = (824, 'lime', lw)
     legend['Omicron Ba.4 - Ba.5 Identified 4/30'] = (830, 'salmon', lw)
     
     if showTotal is True:
